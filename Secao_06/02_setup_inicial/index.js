e231f6272b02fdaa838986080755d39c9a582f8f
Luan Pinto <luan.nelmo@hotmail.com>
2022-11-17 19:52:18 -0300

    Secao 06 #03

--- Secao_06/02_setup_inicial/index.js
+++ Secao_06/02_setup_inicial/index.js
@@ -3,7 +3,52 @@ const app = express()
 const port = 3000
 
 const path = require('path')
+
+app.use(express.urlencoded({
+    extended: true,
+}),
+)
+
+app.use(express.json())
+
 const basePath = path.join(__dirname, 'templates')
+
+const checkAuth = function (req, res, next) {
+    req.authStatus = true
+
+    if (req.authStatus) {
+        console.log('Está logado, pode continuar')
+        next()
+    } else {
+        console.log('Não está logado, faça o login para continuar')
+        next()
+    }
+} 
+
+app.use(checkAuth)
+
+
+app.get('/users/add', (req, res) => {
+    res.sendFile(`${basePath}/userform.html`)
+})
+
+app.post('/users/save', (req, res) => {
+    console.log(req.body)
+
+    const name = req.body.name
+    const age = req.body.age
+
+    console.log(`O nome do usuário é ${name} e tem ${age} anos`);
+
+    res.sendFile(`${basePath}/userform.html`)
+})
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id
+    //  Leitura da tabela users, resgatar um usuário do banco
+    console.log(`Estamos buscando pelo usuário: ${id}`)
+    res.sendFile(`${basePath}/users.html`)
+})
 
 app.get('/', (req, res) => {
     res.sendFile(`${basePath}/index.html`)
