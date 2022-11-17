0981cb56e49b939dd5e07c1b98317528a8a6a603
Luan Pinto <luan.nelmo@hotmail.com>
2022-11-17 19:27:58 -0300

    Secao 06 #02

--- Secao_06/02_setup_inicial/index.js
+++ Secao_06/02_setup_inicial/index.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const path = require('path')
+const basePath = path.join(__dirname, 'templates')
+
 app.get('/', (req, res) => {
-    res.send('Olá Mundo')
+    res.sendFile(`${basePath}/index.html`)
 })
 
 app.listen(port, () => {
