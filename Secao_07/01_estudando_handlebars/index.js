9befc9fd6eb3d706d0b7e1c9f8bbd4c3bf1ace46
Luan Pinto <luan.nelmo@hotmail.com>
2023-01-08 22:39:27 -0300

    Secao_07 #03

--- Secao_07/01_estudando_handlebars/index.js
+++ Secao_07/01_estudando_handlebars/index.js
@@ -2,11 +2,55 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
 
-app.engine('handlebars', exphbs.engine())
+
+const hbs = exphbs.create({
+    partialsDir: ['views/partials'],
+})
+
+app.engine('handlebars', hbs.engine)
 app.set('view engine', 'handlebars')
 
+app.use(express.static('public'))
+
 app.get('/dashboard', (req, res) => {
-    res.render('dashboard')
+    const items = ['item a', 'item b', 'item c']
+
+    res.render('dashboard', { items })
+})
+
+app.get('/post', (req, res) => {
+    const post = {
+        title: 'Aprender Node.js',
+        category: 'JavaScript',
+        body: 'Este artigo vai te ajudar a aprender Node.js...',
+        comments: 4,
+    }
+
+    res.render('blogpost', { post })
+})
+
+app.get('/blog', (req, res) => {
+    const posts = [
+        {
+            title: "Aprender Node.js",
+            category: "JavaScript",
+            body: "Teste",
+            comments: 4
+        },
+        {
+            title: "Aprender CSharp",
+            category: "CSharp",
+            body: "Teste",
+            comments: 4
+        },
+        {
+            title: "Aprender JavaScript",
+            category: "JavaScript",
+            body: "Teste",
+            comments: 4
+        }
+    ]
+    res.render('blog', { posts })
 })
 
 app.get('/', (req, res) => {
